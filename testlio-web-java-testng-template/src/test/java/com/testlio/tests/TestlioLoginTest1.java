b00281d3122c6187c8323dea41d89fe375fc8421
Aleksei Domozhirov <aldomozhirov@gmail.com>
2022-01-18 17:07:23 +0200

    Renamed testlio-android-java-testng-template to testlio-mobile-java-testng-template

---
+++ testlio-web-java-testng-template/src/test/java/com/testlio/tests/TestlioLoginTest1.java
@@ -0,0 +1,47 @@
+package com.testlio.tests;
+
+import com.testlio.lib.BaseTest;
+import com.testlio.pages.TestlioLoginWebPage;
+import org.testng.annotations.Test;
+
+import java.util.ResourceBundle;
+
+import static com.testlio.constants.WebAppUrls.PLATFORM_TESTLIO_QA;
+
+/**
+ * Before running/packaging this test, ensure,
+ * that you created credentials.properties file
+ * in directory src/test/resources with following content:
+ *
+ * user.email=<put your testlio account email>
+ * user.password=<put your testlio account password>
+ */
+public class TestlioLoginTest1 extends BaseTest {
+
+    /**
+     * Test case, verifying, that Testlio user
+     * is able to login to the Testlio platform
+     */
+    @Test
+    public void testlioLoginTest() {
+        // Read credentials from properties file
+        ResourceBundle credentials = ResourceBundle.getBundle("credentials");
+        String userEmail = credentials.getString("user.email");
+        String userPassword = credentials.getString("user.password");
+
+        // Navigate to the initial web application page - TestlioLoginWebPage
+        TestlioLoginWebPage testlioLoginWebPage =
+                (new TestlioLoginWebPage(getWebDriver()))
+                        .navigateTo(PLATFORM_TESTLIO_QA, TestlioLoginWebPage.class);
+
+        // Input user's email address
+        testlioLoginWebPage.inputEmail(userEmail + "test");
+
+        // Input user's password
+        testlioLoginWebPage.inputPassword(userPassword);
+
+        // Click on login button
+        testlioLoginWebPage.clickLogin();
+    }
+
+}
